dc8d35506eb39ec928c24aa704f58c4403693822
LeanNSP <58130895+LeanNSP@users.noreply.github.com>
2021-03-17 22:45:32 +0200

    adds html-css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>Home Book</h1>
+      </header>
+      <main>
+        <p>No list</p>
+        <ul>
+          <li>
+            <p>Emma</p>
+            <p>Hello, my name's Emma</p>
+          </li>
+          <li>
+            <p>Bob</p>
+            <p>Hi, I'm Bob</p>
+          </li>
+          <li>
+            <p>Jack</p>
+            <p>Hello! How are you?</p>
+          </li>
+        </ul>
+      </main>
+      <footer>
+        <form>
+          <label>
+            Name*
+            <input placeholder="Enter a name from 2 to 20 characters" />
+          </label>
+          <label>
+            Message*
+            <input placeholder="Enter a message from 3 to 450 characters" />
+          </label>
+          <button>Add message</button>
+        </form>
+      </footer>
+    </>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
+import "./css/fonts.css";
+import "./css/variablies.css";
+import "./css/reset.css";
+import "./css/style.css";
+
 ReactDOM.render(<App />, document.querySelector("#root"));
