517bcd46b638688777979d3b2dc87b251cc57ec1
LeanNSP <58130895+LeanNSP@users.noreply.github.com>
2021-03-18 10:34:14 +0200

    adds fetchApi

---
+++ src/service/fetchApi.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3300";
+axios.defaults.headers.get["Accept"] = "application/json";
+
+const fetchApi = {
+  post: async (url, reqBody) => {
+    try {
+      const { data } = await axios.post(url, reqBody);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  get: async (url) => {
+    try {
+      const { data } = await axios.get(url);
+
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+export default fetchApi;
